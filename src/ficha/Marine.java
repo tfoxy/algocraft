838bd91a02fad7a416d504abbe64f2b878792869
FontiLucas <fontilucas@hotmail.com>
2015-06-06 19:36:40 -0300

    Update Marine.java

--- src/ficha/Marine.java
+++ src/ficha/Marine.java
@@ -4,6 +4,16 @@ import jugador.TablaJugador;
 
 public class Marine extends Unidad implements FichaTerrestre {
 
+    vida = 40;
+    coste = new Recursos(50, 0, 1))
+    ataque = 6;
+    rangoDeAtaque = 4;
+    transporteMaximo = 1;
+    vision = 7;
+    tiempoDeConstruccion = 3;
+    movimiento = 3;
+
+
     public Marine(TablaJugador jugador) {
         super(jugador);
     }
