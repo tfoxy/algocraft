66857d36420796279f11e16027f3e60d83233225
FontiLucas <fontilucas@hotmail.com>
2015-06-05 22:04:25 -0300

    Implementado la parte lógica. (Puede que falte algún Set.)
    Faltan los Text. Aunque creo que no son necesarios.

--- src/stats/BarrasEscudoVidaEnergia.java
+++ src/stats/BarrasEscudoVidaEnergia.java
@@ -1,5 +1,56 @@
 package stats;
 
 public class BarrasEscudoVidaEnergia {
+	private int vidaMaxima;
+	private int escudoMaxim0;
+	private int energuiaMaxima;
+	
+	private int vidaActual;
+	private int escudoActual;
+	private int energuiaActual;
+	
+	private int vidaPorTurno= 1;
+	private int escudoPorTurno= 10;
+	private int energuiaPorTurno= 10;
+
+	public BarrasEscudoVidaEnergia (int VidaMaxima, int EscudoMaximo, int EnerguiaMaxima)
+	{
+		vidaMaxima = VidaMaxima;
+		escudoMaxim0 = EscudoMaximo;
+		energuiaMaxima = EnerguiaMaxima;
+		vidaActual = VidaMaxima;
+		escudoActual= EscudoMaximo;
+		energuiaActual = EnerguiaMaxima;
+		
+	}
+
+	public void sufrirDaño (int daño, Ficha ficha)
+	{
+		if ( 0>escudoActual - daño)
+		{
+			vidaActual = vidaActual + (escudoActual - daño);
+			escudoActual = 0;
+			if ( 0>= vidaActual)
+			{
+				ficha.Muerete();
+			}
+		}
+		escudoActual = (escudoActual - daño);
+	}
+	
+	public void pasarTurno (int daño, Ficha ficha)
+	{
+		vidaActual = vidaActual + vidaPorTurno;
+		escudoActual= escudoActual + escudoPorTurno;
+		energuiaActual = energuiaActual + energuiaPorTurno;
+		if ( vidaActual > vidaMaxima)
+			vidaActual = vidaMaxima;
+
+		if ( escudoActual > escudoMaximo)
+			escudoActual = escudoMaximo;
+		
+		if ( energuiaActual > energuiaMaxima)
+			energuiaActual = energuiaMaxima;
+	}
 
 }
