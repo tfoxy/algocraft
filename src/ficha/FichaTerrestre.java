60e98eeb3a5ae16385e387b70c907cfb332d42b8
FontiLucas <fontilucas@hotmail.com>
2015-06-07 14:09:46 -0300

    Update FichaTerrestre.java

--- src/ficha/FichaTerrestre.java
+++ src/ficha/FichaTerrestre.java
@@ -1,12 +1,5 @@
 package ficha;
 
-import jugador.TablaJugador;
-
-public class FichaTerrestre extends FichaDeJugador {
-
-	public FichaTerrestre(TablaJugador jugador) {
-		super(jugador);
-		// TODO Auto-generated constructor stub
-	}
+public interface FichaTerrestre extends Ficha {
 
 }
