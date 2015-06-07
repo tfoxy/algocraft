5282e20692cbcbb5067ebc88ed64b54df22de28c
FontiLucas <fontilucas@hotmail.com>
2015-06-07 14:05:00 -0300

    Update FichaAerea.java

--- src/ficha/FichaAerea.java
+++ src/ficha/FichaAerea.java
@@ -1,12 +1,4 @@
 package ficha;
 
-import jugador.TablaJugador;
-
-public class FichaAerea extends FichaDeJugador {
-
-	public FichaAerea(TablaJugador jugador) {
-		super(jugador);
-		// TODO Auto-generated constructor stub
-	}
-
+public interface FichaAerea extends Ficha {
 }
