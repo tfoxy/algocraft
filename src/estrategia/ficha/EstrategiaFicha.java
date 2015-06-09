9aef00ccd4b24d77fe5b4c5d97758c78e0125b8d
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 02:13:18 -0300

    Hacer que compile EstrategiaFicha

--- src/estrategia/ficha/EstrategiaFicha.java
+++ src/estrategia/ficha/EstrategiaFicha.java
@@ -1,41 +1,39 @@
 package estrategia.ficha;
 
 import tablero.Movimiento;
-import Fichas.Ficha;
+import ficha.Ficha;
 import error.NoSePuedeCrearFicha;
 import estrategia.ficha.moduloDeEstrategias.ModuloAtacarYSerAtacado;
 import estrategia.ficha.moduloDeEstrategias.ModuloMorir;
 import estrategia.ficha.moduloDeEstrategias.ModuloMover;
 
 
-
 public abstract class EstrategiaFicha {
 
-	ModuloAtacarYSerAtacado moduloAtacarYSerAtacado = new ModuloAtacarYSerAtacado();
-	ModuloMorir moduloMorir = new ModuloMorir();
+    ModuloAtacarYSerAtacado moduloAtacarYSerAtacado = new ModuloAtacarYSerAtacado();
+    ModuloMorir moduloMorir = new ModuloMorir();
 
-	public void serAtacado ( int danio, Ficha ficha){
-		moduloAtacarYSerAtacado.serAtacado(danio, ficha);
-	}
-	
-	public void matar(Ficha ficha){ 
-		moduloMorir.morir(ficha);
-	}
-
-	
-	
-	
-	public void atacar ( Ficha agresor, Ficha defensor){
-		//nada
-	}
-	
-	public void  PonerEnJuego(Ficha nueva) {
-		//nada
+    public void serAtacado(int danio, Ficha ficha) {
+        moduloAtacarYSerAtacado.serAtacado(danio, ficha);
     }
+
+    public void matar(Ficha ficha) {
+        moduloMorir.morir(ficha);
+    }
+
+
+    public void atacar(Ficha agresor, Ficha defensor) {
+        //nada
+    }
+
+    public void PonerEnJuego(Ficha nueva) {
+        //nada
+    }
+
     public void intentarMovimiento(Ficha ficha, Movimiento dirrecion) {
-		//nada
+        //nada
     }
-	
-	public abstract EstrategiaFicha pasarTurno(Ficha ficha);
+
+    public abstract EstrategiaFicha pasarTurno(Ficha ficha);
 
 }
