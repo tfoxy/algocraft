ed0999d157d6ab8e62b5eebc231feba4164a947b
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 03:10:58 -0300

    Cumplir convención de nombres en Marine

--- src/ficha/Marine.java
+++ src/ficha/Marine.java
@@ -5,12 +5,11 @@ import stats.BarrasEscudoVidaEnergia;
 
 public class Marine extends UnidadTerrestre {
 
-    private static final BarrasEscudoVidaEnergia.Builder barrasBuilder =
-            new BarrasEscudoVidaEnergia.Builder()
-            .vida(40);
+    private static final BarrasEscudoVidaEnergia.Builder BARRAS_BUILDER =
+            new BarrasEscudoVidaEnergia.Builder().vida(40);
 
     public Marine() {
-        barras = barrasBuilder.build();
+        barras = BARRAS_BUILDER.build();
         coste = new Recursos(50, 0, 1);
         ataqueTierra = ataqueAire = 6;
         rangoDeAtaqueTierra = rangoDeAtaqueAire = 4;
