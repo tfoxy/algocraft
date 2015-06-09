24d5b88ce4c21a73e15de471cdd17d81cb5d9143
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 16:37:50 -0300

    Crear TecnologiasInsuficientesException

---
+++ src/error/TecnologiasInsuficientesException.java
@@ -0,0 +1,11 @@
+package error;
+
+public class TecnologiasInsuficientesException extends NoSePuedeCrearFicha {
+
+    public TecnologiasInsuficientesException() {
+    }
+
+    public TecnologiasInsuficientesException(String msg) {
+        super(msg);
+    }
+}
