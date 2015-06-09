b2edc4335b274169d3775643d595d07e9ffd76b4
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 13:11:14 -0300

    Iniciar Asimilador con coste, vida y nombre

--- src/ficha/Asimilador.java
+++ src/ficha/Asimilador.java
@@ -2,8 +2,16 @@ package ficha;
 
 
 import juego.Recursos;
+import stats.BarrasEscudoVidaEnergia;
 
 public class Asimilador extends EdifcioDeRecusosTerrestre {
+
+    public Asimilador() {
+        nombre = "Nexo mineral";
+        coste = new Recursos(100, 0);
+        // TODO agregar tiempo construccion: 5
+        barras = new BarrasEscudoVidaEnergia(450, 450);
+    }
 
     public Recursos extraer() {
         int cantidadExtraida = fuenteDeRecursos.extraer(10);
