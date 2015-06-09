5afea0ff0ab43d17266fd42897a1289d18dbf816
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 14:08:11 -0300

    Iniciar Zealot con nombre, vision, coste, vida y ataque

--- src/ficha/Zealot.java
+++ src/ficha/Zealot.java
@@ -1,8 +1,20 @@
 package ficha;
 
 
+import juego.Recursos;
+import stats.BarrasEscudoVidaEnergia;
+
 public class Zealot extends UnidadTerrestre {
 
-
-
+    public Zealot() {
+        nombre = "Zealot";
+        vision = 7;
+        coste = new Recursos(100, 0);
+        // TODO tiempo construccion
+        barras = new BarrasEscudoVidaEnergia(100, 60);
+        ataqueTierra = 8;
+        rangoDeAtaqueTierra = 1;
+        // TODO ocupa 2 de población
+        // TODO transporte
+    }
 }
