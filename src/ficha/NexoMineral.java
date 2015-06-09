95795063586971cf074b258da9307bbe0e72a281
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 12:56:01 -0300

    Iniciar NexoMineral con coste, vida y nombre.

--- src/ficha/NexoMineral.java
+++ src/ficha/NexoMineral.java
@@ -1,6 +1,15 @@
 package ficha;
 
 
+import juego.Recursos;
+import stats.BarrasEscudoVidaEnergia;
+
 public class NexoMineral extends EdificioTerrestre {
 
+    public NexoMineral() {
+        nombre = "Nexo mineral";
+        coste = new Recursos(50, 0);
+        // TODO agregar tiempo construccion
+        barras = new BarrasEscudoVidaEnergia(250, 250);
+    }
 }
