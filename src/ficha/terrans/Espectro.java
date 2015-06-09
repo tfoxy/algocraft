55f9b8a8bd6bc111b36ec2061b8a070849ce037e
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 14:40:29 -0300

    Iniciar Espectro con nombre, vision, coste, vida y ataque

--- src/ficha/terrans/Espectro.java
+++ src/ficha/terrans/Espectro.java
@@ -1,7 +1,20 @@
 package ficha.terrans;
 
 import ficha.UnidadAerea;
+import juego.Recursos;
+import stats.BarrasEscudoVidaEnergia;
 
 public class Espectro extends UnidadAerea {
 
+    public Espectro() {
+        barras = new BarrasEscudoVidaEnergia(120);
+        coste = new Recursos(150, 100, 2);
+        movimiento = movimientoMaximo = 3;
+        ataqueTierra = 8;
+        ataqueAire = 20;
+        rangoDeAtaqueTierra = rangoDeAtaqueAire = 5;
+        // TODO transporte = 0;
+        vision = 7;
+        tiempoDeConstruccion = 8;
+    }
 }
