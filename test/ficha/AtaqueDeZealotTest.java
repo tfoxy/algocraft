9a1be9914136ebe9fa081c7776d9554ed2dd2a4c
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 03:11:08 -0300

    Hacer que compile AtaqueDeZealotTest

--- test/ficha/AtaqueDeZealotTest.java
+++ test/ficha/AtaqueDeZealotTest.java
@@ -1,6 +1,8 @@
 package ficha;
 
+import estrategia.ficha.moduloDeEstrategias.ModuloAtacarYSerAtacado;
 import juego.Jugador;
+import juego.Raza;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +12,7 @@ import juego.Tecnologia;
 
 public class AtaqueDeZealotTest {
 
-    private EstrategiaAtacarYSerAtacado estrategia;
+    private ModuloAtacarYSerAtacado estrategia;
     private Tablero mapa;
     private FichaTerrestre unidad;
     private FichaTerrestre unidadEnemigaTerrestre;
@@ -21,19 +23,23 @@ public class AtaqueDeZealotTest {
 
     @Before
     public void initialize() {
-        estrategia = new EstrategiaAtacarYSerAtacado();
+        estrategia = new ModuloAtacarYSerAtacado();
 
         int tamanioDeMapa = 40;
 
         mapa = new Tablero(tamanioDeMapa, tamanioDeMapa);
 
-        jugador = new Jugador("miNombre", Tecnologia.PROTOSS);
-        enemigo = new Jugador("miEnemigo", Tecnologia.TERRAN);
+        jugador = new Jugador("miNombre", Raza.PROTOSS);
+        enemigo = new Jugador("miEnemigo", Raza.TERRAN);
 
-        unidad = new Zealot(jugador);
+        unidad = new Zealot();
+        jugador.asignar(unidad);
 
-        unidadEnemigaTerrestre = new Marine(jugador);
-        unidadEnemigaAerea = new Espectro(jugador);
+        unidadEnemigaTerrestre = new Marine();
+        unidadEnemigaAerea = new Espectro();
+
+        enemigo.asignar(unidadEnemigaTerrestre);
+        enemigo.asignar(unidadEnemigaAerea);
     }
 
 
@@ -43,7 +49,7 @@ public class AtaqueDeZealotTest {
         mapa.insertar(new Coordenada(3, 4), unidadEnemigaTerrestre);
 
         boolean fueAtacado =
-                estrategia.atacado(unidad, unidadEnemigaTerrestre);
+                estrategia.atacar(unidad, unidadEnemigaTerrestre);
 
         Assert.assertTrue(fueAtacado);
     }
@@ -55,7 +61,7 @@ public class AtaqueDeZealotTest {
         mapa.insertar(new Coordenada(3, 4), unidadEnemigaAerea);
 
         boolean fueAtacado =
-                estrategia.atacado(unidad, unidadEnemigaAerea);
+                estrategia.atacar(unidad, unidadEnemigaAerea);
 
         Assert.assertFalse(fueAtacado);
     }
@@ -67,7 +73,7 @@ public class AtaqueDeZealotTest {
         mapa.insertar(new Coordenada(3, 3), unidadEnemigaAerea);
 
         boolean fueAtacado =
-                estrategia.atacado(unidad, unidadEnemigaAerea);
+                estrategia.atacar(unidad, unidadEnemigaAerea);
 
         Assert.assertFalse(fueAtacado);
     }
@@ -79,7 +85,7 @@ public class AtaqueDeZealotTest {
         mapa.insertar(new Coordenada(3, 5), unidadEnemigaTerrestre);
 
         boolean fueAtacado =
-                estrategia.atacado(unidad, unidadEnemigaTerrestre);
+                estrategia.atacar(unidad, unidadEnemigaTerrestre);
 
         Assert.assertFalse(fueAtacado);
     }
