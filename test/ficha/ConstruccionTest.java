33eaaaa56f6dd32924c71a7109a2bfeef9d4dae8
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 03:43:23 -0300

    Hacer que compile ConstruccionTest

--- test/ficha/ConstruccionTest.java
+++ test/ficha/ConstruccionTest.java
@@ -1,12 +1,37 @@
 package ficha;
 
+import juego.Jugador;
+import juego.Raza;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ConstruccionTest {
 
+    private Jugador protoss;
 
+    @Before
+    public void initialize() {
+        protoss = new Jugador("Poroto", Raza.PROTOSS, 500, 200);
+    }
 
     @Test
-    public void
+    public void queTardeElTiempoCorrecto() {
+        Pilon pilon = new Pilon();
+
+        protoss.construir(pilon);
+
+        // TODO usar getter para tiempo de pilon y usar for
+        pilon.pasarTurno();
+        pilon.pasarTurno();
+        pilon.pasarTurno();
+        pilon.pasarTurno();
+        pilon.pasarTurno();
+
+        assertEquals(protoss.poblacionPosible(), 5);
+    }
+
+    // TODO hacer tests
 
 }
