d432711635121dca468b7a2ae796f16129ea44b9
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 15:14:23 -0300

    Agregar test tieneEspacioTerrestreAlCrearse a CasillaTest

---
+++ test/juego/RecursosDeJugadorTest.java
@@ -0,0 +1,45 @@
+package juego;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecursosDeJugadorTest {
+
+    private RecursosDeJugador recursosDeJugador;
+
+    @Before
+    public void initialize() {
+        recursosDeJugador = new RecursosDeJugador(50, 50, 100);
+    }
+
+    @Test
+    public void tieneSuficientesRecursos() {
+        Recursos coste = new Recursos(10, 20);
+        boolean hayRecursos =
+                recursosDeJugador.haySuficienteRecursos(coste);
+
+        Assert.assertTrue(hayRecursos);
+    }
+
+    @Test
+    public void noTieneSuficientesRecursosPorFaltaDeGas() {
+        Recursos coste = new Recursos(10, 100);
+        boolean hayRecursos =
+                recursosDeJugador.haySuficienteRecursos(coste);
+
+        Assert.assertFalse(hayRecursos);
+    }
+
+    @Test
+    public void noTieneSuficientesRecursosPorFaltaDeMineral() {
+        Recursos coste = new Recursos(100, 10);
+        boolean hayRecursos =
+                recursosDeJugador.haySuficienteRecursos(coste);
+
+        Assert.assertFalse(hayRecursos);
+    }
+
+
+
+}
