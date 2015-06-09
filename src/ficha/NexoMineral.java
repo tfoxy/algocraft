f04a0db8f47c89e5b45dd07aac1e5e1b93502414
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 13:11:29 -0300

    Agregar método extraer a NexoMineral

--- src/ficha/NexoMineral.java
+++ src/ficha/NexoMineral.java
@@ -9,7 +9,13 @@ public class NexoMineral extends EdificioTerrestre {
     public NexoMineral() {
         nombre = "Nexo mineral";
         coste = new Recursos(50, 0);
-        // TODO agregar tiempo construccion
+        // TODO agregar tiempo construccion: 4
         barras = new BarrasEscudoVidaEnergia(250, 250);
     }
+
+    public Recursos extraer() {
+        int cantidadExtraida = fuenteDeRecursos.extraer(10);
+
+        return new Recursos(0, cantidadExtraida);
+    };
 }
