cc433bdd8b4d7f87eb9c7ffd6b822bcdbe9ff237
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-09 16:01:19 -0300

    Crear MovimientoInsuficienteException

---
+++ src/error/MovimientoInsuficienteException.java
@@ -0,0 +1,12 @@
+package error;
+
+public class MovimientoInsuficienteException extends JuegoException {
+
+    public MovimientoInsuficienteException() {
+    }
+
+    public MovimientoInsuficienteException(String msg) {
+        super(msg);
+    }
+
+}
