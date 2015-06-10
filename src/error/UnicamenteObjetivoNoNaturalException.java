ac32ea8650012eb9b96e1961accae4a2d6dd0fff
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-10 12:17:41 -0300

    Crear UnicamenteObjetivoNoNaturalException

---
+++ src/error/UnicamenteObjetivoNoNaturalException.java
@@ -0,0 +1,10 @@
+package error;
+
+public class UnicamenteObjetivoNoNaturalException extends JuegoException {
+    public UnicamenteObjetivoNoNaturalException() {
+    }
+
+    public UnicamenteObjetivoNoNaturalException(String msg) {
+        super(msg);
+    }
+}
