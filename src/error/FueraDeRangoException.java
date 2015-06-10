60b59ee7b77af4b99f2c39c598ea4bfcaece33cd
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-10 09:26:16 -0300

    Crear FueraDeRangoException

---
+++ src/error/FueraDeRangoException.java
@@ -0,0 +1,10 @@
+package error;
+
+public class FueraDeRangoException extends JuegoException {
+    public FueraDeRangoException() {
+    }
+
+    public FueraDeRangoException(String msg) {
+        super(msg);
+    }
+}
