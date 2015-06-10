e5acc6043d5ec8de9b37ad78617702f0b924becf
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-10 14:05:12 -0300

    Corregir ConstruccionOP

--- src/estrategia/ficha/ExtrategiaConstrucccionOP.java
+++ src/estrategia/ficha/ExtrategiaConstrucccionOP.java
@@ -2,43 +2,49 @@ package estrategia.ficha;
 
 import estrategia.ficha.moduloDeEstrategias.ModuloConstruccion;
 import estrategia.ficha.moduloDeEstrategias.ModuloConstruccionOP;
-import ficha.CasaTerrestre;
-import ficha.EdifcioDeRecusosTerrestre;
-import ficha.EdificioTerrestre;
-import ficha.Ficha;
-import ficha.FichaTerrestre;
-import ficha.FuenteDeRecurso;
-import ficha.UnidadAerea;
-import ficha.UnidadTerrestre;
+import ficha.*;
 
-public class ExtrategiaConstrucccionOP{
+public class ExtrategiaConstrucccionOP {
 
 
     private ModuloConstruccionOP moduloConstruccion = new ModuloConstruccionOP();
 
 
+    /* TODO implementar
     public void PonerEnJuego(Ficha nueva) {
+
     }
+    */
+
     public void PonerEnJuego(FichaTerrestre nueva) {
         moduloConstruccion.PonerEnJuego(nueva);
         nueva.propietario().agregarPoblacionTotal(nueva.poblacionQueDa());
     }
+
+    public void PonerEnJuego(FichaAerea nueva) {
+        moduloConstruccion.PonerEnJuego(nueva);
+        nueva.propietario().agregarPoblacionTotal(nueva.poblacionQueDa());
+    }
+
     public void PonerEnJuego(FuenteDeRecurso nueva) {
         moduloConstruccion.PonerEnJuego(nueva);
         nueva.propietario().agregarPoblacionTotal(nueva.poblacionQueDa());
     }
+
     public void PonerEnJuego(EdifcioDeRecusosTerrestre nueva) {
         moduloConstruccion.PonerEnJuego(nueva);
         nueva.propietario().agregarPoblacionTotal(nueva.poblacionQueDa());
     }
+
     public void PonerEnJuego(UnidadTerrestre nueva) {
         moduloConstruccion.PonerEnJuego(nueva);
         nueva.propietario().agregarPoblacionTotal(nueva.poblacionQueDa());
     }
-    public void PonerEnJuego(UnidadAerea  nueva) {
+
+    public void PonerEnJuego(UnidadAerea nueva) {
         moduloConstruccion.PonerEnJuego(nueva);
         nueva.propietario().agregarPoblacionTotal(nueva.poblacionQueDa());
     }
-    
+
 
 }
--- src/estrategia/ficha/moduloDeEstrategias/ModuloConstruccionOP.java
+++ src/estrategia/ficha/moduloDeEstrategias/ModuloConstruccionOP.java
@@ -1,29 +1,17 @@
 package estrategia.ficha.moduloDeEstrategias;
 
 import error.NoSePuedeCrearFicha;
-import ficha.EdifcioDeRecusosTerrestre;
-import ficha.Ficha;
-import ficha.FichaTerrestre;
-import ficha.FuenteDeRecurso;
-import ficha.UnidadAerea;
-import ficha.UnidadTerrestre;
+import ficha.*;
 
 public class ModuloConstruccionOP {
 
+    /* TODO implementar
     public void PonerEnJuego(Ficha nueva) {
         // para el polimorfismo.
     }
+    */
 //No importan. Los recursos ni la Tecnologia.. es un Digamos Word Editor
 
-    
-    // Fichaterrestre.
-    
-    public void PonerEnJuego(FichaTerrestre nueva) {
-        if (this.sePuedeCrear(nueva)) {
-            nueva.propietario().newFicha2(nueva);
-            nueva.tablero().insertar2(nueva.coordenada(), nueva);
-        }
-    }
 
     public boolean sePuedeCrear(FichaTerrestre nueva) throws NoSePuedeCrearFicha {
         if (!nueva.tablero().hayEspacioTerreste(nueva.coordenada())) {
@@ -31,44 +19,55 @@ public class ModuloConstruccionOP {
         }
         return true;
     }
-    // Fichaterrestre.
 
-    // UnidadTerrestre.
-    
-    public void PonerEnJuego(UnidadTerrestre nueva) {
-        if (this.sePuedeCrear(nueva)) {
-        	nueva.propietario().recursos().poblacion().aumentarActualForzadamente(nueva.coste().poblacion());
-            nueva.propietario().newFicha2(nueva);
-            nueva.tablero().insertar2(nueva.coordenada(), nueva);
-        }
-    }
-
-    public boolean sePuedeCrear(UnidadTerrestre nueva) throws NoSePuedeCrearFicha {
-        if (!nueva.tablero().hayEspacioTerreste(nueva.coordenada())) {
-            throw new NoSePuedeCrearFicha("Espacio Ocupado");
-        }
-        return true;
-    }
-    // UnidadTerrestre.
-    
-    // UnidadArrea.
-    
-    public void PonerEnJuego(UnidadAerea nueva) {
-        if (this.sePuedeCrear(nueva)) {
-        	nueva.propietario().recursos().poblacion().aumentarActualForzadamente(nueva.coste().poblacion());
-            nueva.propietario().newFicha2(nueva);
-            nueva.tablero().insertar2(nueva.coordenada(), nueva);
-        }
-    }
-
-    public boolean sePuedeCrear(UnidadAerea nueva) throws NoSePuedeCrearFicha {
+    public boolean sePuedeCrear(FichaAerea nueva) throws NoSePuedeCrearFicha {
         if (!nueva.tablero().hayEspacioArreo(nueva.coordenada())) {
             throw new NoSePuedeCrearFicha("Espacio Ocupado");
         }
         return true;
     }
+
+    // Fichaterrestre.
+
+    public void PonerEnJuego(FichaTerrestre nueva) {
+        if (this.sePuedeCrear(nueva)) {
+            nueva.propietario().newFicha2(nueva);
+            nueva.tablero().insertar2(nueva.coordenada(), nueva);
+        }
+    }
+    // Fichaterrestre.
+
+    // UnidadTerrestre.
+
+    public void PonerEnJuego(UnidadTerrestre nueva) {
+        if (this.sePuedeCrear(nueva)) {
+            nueva.propietario().recursos().poblacion().aumentarActualForzadamente(nueva.coste().poblacion());
+            nueva.propietario().newFicha2(nueva);
+            nueva.tablero().insertar2(nueva.coordenada(), nueva);
+        }
+    }
+
+    // FichaAerea.
+
+    public void PonerEnJuego(FichaAerea nueva) {
+        if (this.sePuedeCrear(nueva)) {
+            nueva.propietario().newFicha2(nueva);
+            nueva.tablero().insertar2(nueva.coordenada(), nueva);
+        }
+    }
+    // FichaAerea.
+
     // UnidadArrea.
-    
+
+    public void PonerEnJuego(UnidadAerea nueva) {
+        if (this.sePuedeCrear(nueva)) {
+            nueva.propietario().recursos().poblacion().aumentarActualForzadamente(nueva.coste().poblacion());
+            nueva.propietario().newFicha2(nueva);
+            nueva.tablero().insertar2(nueva.coordenada(), nueva);
+        }
+    }
+    // UnidadArrea.
+
     //recursoTerrestre
     public void PonerEnJuego(FuenteDeRecurso nueva) {
         if (this.sePuedeCrear(nueva)) {
@@ -76,16 +75,8 @@ public class ModuloConstruccionOP {
             nueva.tablero().insertar2(nueva.coordenada(), nueva);
         }
     }
-    
-    public boolean sePuedeCrear(FuenteDeRecurso nueva) throws NoSePuedeCrearFicha {
-        if (!nueva.tablero().hayEspacioTerreste(nueva.coordenada())) {
-            throw new NoSePuedeCrearFicha("Espacio Ocupado");
-        }
-        return true;
-    }
-
     //recursoTerrestre
-    
+
     //ExtrearRecursos
     public boolean sePuedeCrear(EdifcioDeRecusosTerrestre nueva) throws NoSePuedeCrearFicha {
         Ficha recurso = nueva.tablero().getCasilla(nueva.coordenada()).getFichaTerrestre();
@@ -99,13 +90,13 @@ public class ModuloConstruccionOP {
     public void PonerEnJuego(EdifcioDeRecusosTerrestre nueva) { //aun asi Se tiene que Crear la Fuente de Recursos
         if (this.sePuedeCrear(nueva)) {
             nueva.propietario().newFicha2(nueva);
-            
+
             FuenteDeRecurso fuenteDeRecursos = (FuenteDeRecurso) nueva.tablero().getCasilla(nueva.coordenada()).getFichaTerrestre(); //si no puedo quitar el casteo
             nueva.tablero().getCasilla(nueva.coordenada()).eliminarFichaTerrestre();
-            
+
             nueva.fuenteDeRecursos(fuenteDeRecursos);
             nueva.tablero().insertar2(nueva.coordenada(), nueva);
-            
+
         }
     }
     //ExtrearRecursos
