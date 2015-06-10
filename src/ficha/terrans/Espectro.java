2e6db8bd6a494bf40ba59b831af85d31848cd9e9
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-10 08:58:00 -0300

    Utilizar Ataque en Zealot y Espectro

--- src/ficha/terrans/Espectro.java
+++ src/ficha/terrans/Espectro.java
@@ -2,6 +2,7 @@ package ficha.terrans;
 
 import ficha.UnidadAerea;
 import juego.Recursos;
+import stats.Ataque;
 import stats.BarrasEscudoVidaEnergia;
 
 public class Espectro extends UnidadAerea {
@@ -11,9 +12,8 @@ public class Espectro extends UnidadAerea {
         barras = new BarrasEscudoVidaEnergia(120);
         coste = new Recursos(150, 100, 2);
         movimiento = movimientoMaximo = 3;
-        ataqueTierra = 8;
-        ataqueAire = 20;
-        rangoDeAtaqueTierra = rangoDeAtaqueAire = 5;
+        ataqueTierra = new Ataque(8, 5);
+        ataqueAire = new Ataque(20, 5);
         // TODO transporte = 0;
         vision = 7;
         tiempoDeConstruccion = 8;
