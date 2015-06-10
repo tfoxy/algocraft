4988d1a9106afb97e15e21742e155578e17a85cd
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-10 08:46:37 -0300

    Utilizar Ataque en Ficha y Marine

--- src/ficha/terrans/Marine.java
+++ src/ficha/terrans/Marine.java
@@ -2,6 +2,7 @@ package ficha.terrans;
 
 import ficha.UnidadTerrestre;
 import juego.Recursos;
+import stats.Ataque;
 import stats.BarrasEscudoVidaEnergia;
 
 public class Marine extends UnidadTerrestre {
@@ -12,8 +13,7 @@ public class Marine extends UnidadTerrestre {
     public Marine() {
         barras = BARRAS_BUILDER.build();
         coste = new Recursos(50, 0, 1);
-        ataqueTierra = ataqueAire = 6;
-        rangoDeAtaqueTierra = rangoDeAtaqueAire = 4;
+        ataqueTierra = ataqueAire = new Ataque(6, 4);
         // TODO transporteMaximo = 1;
         vision = 7;
         tiempoDeConstruccion = 3;
