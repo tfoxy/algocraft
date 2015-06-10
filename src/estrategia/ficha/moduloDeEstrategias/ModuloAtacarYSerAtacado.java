ddffa0cc2c8628464c9012f2527e7f04816d5c7c
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-10 09:10:53 -0300

    Utilizar tipoDeAtaqueRecibido en ModuloAtacarYSerAtacado

--- src/estrategia/ficha/moduloDeEstrategias/ModuloAtacarYSerAtacado.java
+++ src/estrategia/ficha/moduloDeEstrategias/ModuloAtacarYSerAtacado.java
@@ -3,54 +3,33 @@ package estrategia.ficha.moduloDeEstrategias;
 import ficha.Ficha;
 import ficha.FichaAerea;
 import ficha.FichaTerrestre;
+import stats.Ataque;
 import tablero.Coordenada;
 
 
 public class ModuloAtacarYSerAtacado {
 
-    private int alcanse;
-
     public void serAtacado(int danio, Ficha defensor) {
         defensor.barras().sufrirDanio(danio, defensor);
     }
 
+
     public boolean atacar(Ficha agresor, Ficha defensor) {
-        return false;
-        //para el polimorfismo
-    }
+        final Ataque ataque = defensor.tipoDeAtaqueRecibido(agresor);
 
-
-    public boolean atacar(Ficha agresor, FichaTerrestre defensor) {
-
-        alcanse = agresor.rangoDeAtaqueTierra();
-        if (this.puedoAtacar(agresor, defensor)) {
-            this.serAtacado(agresor.ataqueTierra(), defensor);
+        if (this.puedoAtacar(agresor, defensor, ataque.rango())) {
+            this.serAtacado(ataque.danio(), defensor);
             return true;
         }
         return false;
     }
 
-    public boolean atacar(Ficha agresor, FichaAerea defensor) {
 
-        alcanse = agresor.rangoDeAtaqueAire();
-        if (this.puedoAtacar(agresor, defensor)) {
-            this.serAtacado(agresor.ataqueAire(), defensor);
-            return true;
-        }
-        return false;
-    }
-    //se puede ahoraar unas 3 lineas mas de codigo... pero ya qudaria confuso.
-
-
-    private boolean puedoAtacar(Ficha agresor, Ficha defensor) {
-
+    private boolean puedoAtacar(Ficha agresor, Ficha defensor, int rango) {
         Coordenada posicionAgresor = agresor.coordenada();
         Coordenada posicionDefensor = defensor.coordenada();
 
-        if (alcanse >= posicionAgresor.distanciaAObjetivo(posicionDefensor)) {
-            return true;
-        }
-        return false;
+        return rango >= posicionAgresor.distanciaAObjetivo(posicionDefensor);
     }
 }
 
