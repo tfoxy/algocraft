5253e7c20a83852032ae9b9d17c5b6914a8a1335
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-11 12:19:06 -0300

    Refactorizar CoordenadasVisibles

--- src/tablero/CoordenadasVisibles.java
+++ src/tablero/CoordenadasVisibles.java
@@ -5,30 +5,14 @@ import java.util.Set;
 
 public class CoordenadasVisibles {
 
-    private Set<Coordenada> coordenadaVisibles = new HashSet<>();
-    
-    private void agregar(Coordenada coordenada) {
-    	coordenadaVisibles.add(coordenada);
-    }
+    private Set<Coordenada> coordenadasVisibles = new HashSet<>();
 
     public void verDesdeEstePunto(Coordenada coordenada, int vision) {
-        verRecursivamente(coordenada, vision);
-    }
-
-    private void verRecursivamente(Coordenada coordenada, int vision) {
-        agregar(coordenada);
-
-        if (vision > 0) {
-            int siguienteVision = vision - 1;
-            verRecursivamente(coordenada.dameCordenadaHacia(Direccion.ARRIBA), siguienteVision);
-            verRecursivamente(coordenada.dameCordenadaHacia(Direccion.ABAJO), siguienteVision);
-            verRecursivamente(coordenada.dameCordenadaHacia(Direccion.DERECHA), siguienteVision);
-            verRecursivamente(coordenada.dameCordenadaHacia(Direccion.IZQUIERDA), siguienteVision);
-        }
+        coordenadasVisibles = CoordenadaUtil.areaDeCoordenadas(coordenada, vision);
     }
 
     public boolean puedeVer(Coordenada coordenada) {
-        return coordenadaVisibles.contains(coordenada);
+        return coordenadasVisibles.contains(coordenada);
     }
 
 	
