9b403c0f3185a72a158da12a58337684e5a80854
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-11 23:08:03 -0300

    Crear clase EdificioDeRecursosNecesitaFichaRecursoException

---
+++ src/error/EdificioDeRecursosNecesitaFichaRecursoException.java
@@ -0,0 +1,10 @@
+package error;
+
+public class EdificioDeRecursosNecesitaFichaRecursoException extends NoSePuedeCrearFicha {
+    public EdificioDeRecursosNecesitaFichaRecursoException() {
+    }
+
+    public EdificioDeRecursosNecesitaFichaRecursoException(String msg) {
+        super(msg);
+    }
+}
