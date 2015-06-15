bc689feeab18292efb1d2198dd5488130b56e9ea
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-15 20:18:00 -0300

    Crear TipoDeFicha

---
+++ src/ficha/TipoDeFicha.java
@@ -0,0 +1,15 @@
+package ficha;
+
+import java.util.EnumSet;
+
+public enum TipoDeFicha {
+    VACIA,
+    TERRENO,
+    UNIDAD,
+    EDIFICIO,
+    RECURSO,
+    VOLCAN,
+    MINERAL,
+    TERRESTRE,
+    AEREA
+}
