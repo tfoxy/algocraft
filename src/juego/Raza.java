68a4bdbbbbf0866823c5df2aa799daa3ff863a85
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-17 13:56:55 -0300

    Agregar Raza.nuevaCasa()

--- src/juego/Raza.java
+++ src/juego/Raza.java
@@ -1,34 +1,51 @@
 package juego;
 
 import ficha.Ficha;
+import ficha.protoss.edificio.Pilon;
 import ficha.protoss.unidad.Zealot;
+import ficha.terran.edificio.DepositoSuministro;
 import ficha.terran.unidad.Marine;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public enum Raza {
-    TERRAN(Marine.class, Tecnologia.TERRAN),
-    PROTOSS(Zealot.class, Tecnologia.PROTOSS);
+    TERRAN,
+    PROTOSS;
+
+    static {
+        TERRAN.tecnologiasIniciales = Collections.singletonList(Tecnologia.TERRAN);
+        TERRAN.unidadBasicaClass = Marine.class;
+        TERRAN.casaClass = DepositoSuministro.class;
+
+        PROTOSS.tecnologiasIniciales = Collections.singletonList(Tecnologia.PROTOSS);
+        PROTOSS.unidadBasicaClass = Zealot.class;
+        PROTOSS.casaClass = Pilon.class;
+    }
+
 
     private Class<? extends Ficha> unidadBasicaClass;
+    private Class<? extends Ficha> casaClass;
     private List<Tecnologia> tecnologiasIniciales;
-
-    Raza(Class<? extends Ficha> unidadBasicaClass, Tecnologia... tecnologiasIniciales) {
-        this.unidadBasicaClass = unidadBasicaClass;
-        this.tecnologiasIniciales = Arrays.asList(tecnologiasIniciales);
-    }
 
     public List<Tecnologia> tecnologiasIniciales() {
         return tecnologiasIniciales;
     }
 
-    public Ficha nuevaUnidadBasica() {
+    private Ficha newInstance(Class<? extends Ficha> c) {
         try {
-            return unidadBasicaClass.newInstance();
+            return c.newInstance();
         } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
             throw new RuntimeException(e.getMessage(), e.getCause());
         }
+    }
+
+    public Ficha nuevaUnidadBasica() {
+        return newInstance(unidadBasicaClass);
+    }
+
+    public Ficha nuevaCasa() {
+        return newInstance(casaClass);
     }
 }
