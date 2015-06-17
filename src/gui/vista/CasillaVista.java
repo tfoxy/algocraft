3afa0739a9c582ef63ff1f9954dfe28d2a0a79b0
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-17 19:19:19 -0300

    Separar aire y tierra en CasillaVista

--- src/gui/vista/CasillaVista.java
+++ src/gui/vista/CasillaVista.java
@@ -4,48 +4,72 @@ import ficha.Ficha;
 import gui.modelo.TableroObservable;
 import tablero.Altura;
 import tablero.Coordenada;
+import tablero.Coordenada3d;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.util.Observer;
 
 public class CasillaVista extends JPanel {
 
-    private Ficha fichaTerrestre;
-    private Ficha fichaAerea;
+    private CasillaParaFicha tierra;
+    private CasillaParaFicha aire;
+
+    private class CasillaParaFicha extends JPanel {
+        private Ficha ficha;
+
+        private CasillaParaFicha(Ficha ficha) {
+            this.ficha = ficha;
+        }
+
+        @Override
+        public void paintComponent(Graphics grafico) {
+            super.paintComponent(grafico);
+            grafico.setColor(ficha.miColor());
+
+            Dimension dimension = getSize();
+
+            grafico.fillRect(0, 0, dimension.width, dimension.height);
+
+            setToolTipText(ficha.nombre() + " " + ficha.coordenada().proyeccion().toString());
+        }
+    }
 
     public CasillaVista(Coordenada coordenada, TableroObservable mapa) {
-        this.fichaTerrestre = mapa.getFichaTerrestre(coordenada);
+        tierra = new CasillaParaFicha(mapa.getFichaTerrestre(coordenada));
+        aire = new CasillaParaFicha(mapa.getFichaAerea(coordenada));
 
         Observer observer = crearObserver(coordenada);
         mapa.addObserver(observer);
+
+        setLayout(new GridLayout(2, 1));
+        setBorder(BorderFactory.createRaisedSoftBevelBorder());
+
+        add(aire);
+        add(tierra);
     }
 
-    private void setFicha(Ficha ficha) {
+    private void repaintWith(Ficha ficha) {
+        CasillaParaFicha casillaParaFicha;
+
         if (ficha.coordenada().z == Altura.TIERRA)
-            fichaTerrestre = ficha;
+            casillaParaFicha = tierra;
         else
-            fichaAerea = ficha;
-    }
+            casillaParaFicha = aire;
 
-    @Override
-    public void paintComponent(Graphics grafico) {
-        super.paintComponent(grafico);
-        grafico.setColor(fichaTerrestre.miColor());
-        Dimension dimension = getSize();
-
-        grafico.fill3DRect(0, 0, dimension.width, dimension.height, true);
-
-        setToolTipText(fichaTerrestre.nombre() + " " + fichaTerrestre.coordenada().proyeccion().toString());
+        casillaParaFicha.ficha = ficha;
+        casillaParaFicha.repaint();
     }
 
     private Observer crearObserver(Coordenada coordenada) {
         return new CasillaObserver(coordenada) {
             @Override
             protected void updateVista(Ficha fichaNueva) {
-                setFicha(fichaNueva);
-                repaint();
+                repaintWith(fichaNueva);
             }
         };
     }
