ef34db20a36bade8ed077661517d8d4c5db8ecb4
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-20 17:23:46 -0300

    Agregar Refineria

---
+++ src/ficha/terran/edificio/Refineria.java
@@ -0,0 +1,21 @@
+package ficha.terran.edificio;
+
+import ficha.EdifcioDeRecursosTerrestre;
+import ficha.TipoDeFicha;
+import juego.Recursos;
+import juego.Tecnologia;
+import stats.BarrasEscudoVidaEnergia;
+
+public class Refineria extends EdifcioDeRecursosTerrestre {
+    public Refineria() {
+        nombre = "Refinería";
+        coste = new Recursos(100, 0);
+        turnosParaCrear = 6;
+        barras = new BarrasEscudoVidaEnergia(750);
+        tecnologiasNecesarias.add(Tecnologia.TERRAN);
+
+        recursosExtraidosPorTurno = new Recursos(0, 10);
+
+        tipoDeFichaNecesaria = TipoDeFicha.VOLCAN;
+    }
+}
