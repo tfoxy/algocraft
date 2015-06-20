44f5d6d31e2527ae2ac4776b10b2bfdc742a8d3e
Tomás Fox <tomas.c.fox@gmail.com>
2015-06-20 16:31:44 -0300

    Agregar color a TerrenoEspacial

--- src/ficha/natural/terreno/TerrenoEspacial.java
+++ src/ficha/natural/terreno/TerrenoEspacial.java
@@ -3,10 +3,17 @@ package ficha.natural.terreno;
 import ficha.FichaTerrestre;
 import ficha.TipoDeFicha;
 
+import java.awt.Color;
+
 public class TerrenoEspacial extends FichaTerrestre {
 
     public TerrenoEspacial() {
         this.tipoDeFicha.add(TipoDeFicha.TERRENO);
         this.tipoDeFicha.add(TipoDeFicha.NATURAL);
+    }
+
+    @Override
+    public Color miColor() {
+        return Color.GRAY;
     }
 }
